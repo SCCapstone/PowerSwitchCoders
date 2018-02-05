b2d66c1007884f0244ba3e6c84754b4bed90e361
William Smith <wlsmith@email.sc.edu>
2018-02-04 19:06:09 -0500

    Testing Update 1/2
    Added very simple Edit Mode Tests to cover Unit Testing part of Milestone; Behavioral tests are WIP but will be run through Window->Test Runner-> Play Mode-> Run All

---
+++ PowerSwitch2D/Assets/Editor/SimplePlayModeTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class SimplePlayModeTest {
+
+	[Test]
+	public void SimplePlayModeTestSimplePasses() {
+		// Use the Assert class to test conditions.
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator SimplePlayModeTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+
+        var currentVehicle = GameObject.FindWithTag("Vehicle");
+        var currentSpeed = currentVehicle.GetComponent<FollowPath>().Speed;
+
+        //yield return new WaitForSeconds(5.0f);
+        yield return null;
+
+        Assert.Greater(currentSpeed, 0.0f);
+
+		
+	}
+
+    //[UnityTest]
+    //Unity behavioral test ensuring that the FollowPath.cs code reliably finds the next point in an editor-defined path
+    //public IEnumerator CanFindNextPointOnPath()
+    //{
+        
+    //    yield return null;
+    //}
+}
---
+++ PowerSwitch2D/Assets/Tests/PlayModeTester.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Tests
+{ 
+    public class PathfinderTests
+    {
+        [UnityTest]
+        public IEnumerator CanFindNextPathPoint()
+        {
+            var basePath = Resources.Load("Tests/CarPath");
+            var currentPath = new GameObject().AddComponent<MovementPath>();
+            var pathScript = currentPath.GetComponent<MovementPath>();
+            var pointList = pathScript.PathSequence;
+
+            yield return new WaitForSeconds(5.0f);
+
+            Assert.GreaterOrEqual(1, pointList.Length);
+        }
+    }
+}
