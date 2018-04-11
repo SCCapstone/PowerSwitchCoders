3f92617241956ba0961a64633a242ad96f3a8e39
William Smith <wlsmith@email.sc.edu>
2018-04-10 20:47:15 -0400

    Pre-RC1 Update
    Path Pick part of algorithm is finished, along with Winning Path Calculator (only player choice based atm)

--- PowerSwitch2D/Assets/Scripts/TheMagicAlgorithm.cs
+++ PowerSwitch2D/Assets/Scripts/TheMagicAlgorithm.cs
@@ -25,13 +25,24 @@ public class TheMagicAlgorithm : MonoBehaviour {
 
     private string[] vehicleInts = { "Dummy", "Bike", "Car", "Boat", "Train" };
     private string[] pathJoints = { "AB", "BC", "CD", "DE", "EF", "FG" };
+    private string[] fuelTypes = { "Dummy", "man", "oil", "wind", "coal" };
+    //will therefore need a checkUpgrades function 
 
     private int pCursor = 0;
+
+    [Header("Number of Power Points")]
+    [Tooltip("Must be greater than zero")]
+
+    public int powerPoints = 100;
 
     [Header("Number of Choices = playerPaths size")]
     [Tooltip("Determines size of array playerPaths")]
 
     public MovementPath[] playerPaths;
+    private int[] playerFuels;
+
+    public string usertest { private get; set; }
+    //private string[] powerSources;
 
     //private MovementPath[] possPaths;
 
@@ -45,6 +56,8 @@ public class TheMagicAlgorithm : MonoBehaviour {
     void Start () {
 
         RandomizeCosts();
+        playerFuels = new int[playerPaths.Length];
+        //powerSources = new string[playerPaths.Length];
         //pCursor = 1;
 
         //int tempSize = playerPaths.Length;
@@ -80,14 +93,27 @@ public class TheMagicAlgorithm : MonoBehaviour {
     public void PickPath(int choice)
     {
         string vPick = vehicleInts[choice];
-        Debug.Log(vPick);
+        //Debug.Log(vPick);
+
+
         MovementPath nextPath = PathFinder(vPick);
         playerPaths.SetValue(nextPath, pCursor);
+
+        string fuelChoice = fuelTypes[choice];
+        int nextFuel = FuelSetter(fuelChoice);
+        playerFuels.SetValue(nextFuel, pCursor);
+        Debug.Log(fuelChoice+nextFuel);
+
+        //int nextFuel = FuelSetter(fuel);
+        //playerFuels.SetValue(nextFuel, pCursor);
+        
 
         pCursor++;
         if (pCursor >= playerPaths.Length)
         {
             Debug.Log("Picked Max Number of Points");
+            CheckWinningPath();
+            //Debug.Log()
             //Player is done picking, calculate if their path will win or not using above IF
             //Also activate car sprite and set to correct value based on path, but don't move it yet
         }
@@ -112,4 +138,62 @@ public class TheMagicAlgorithm : MonoBehaviour {
         MovementPath newMove = tempPath.GetComponent<MovementPath>();
         return newMove;
     }
+
+    //This.... not great 
+    int FuelSetter(string fuelType)
+    {
+        if (fuelType == null)
+        {
+            Debug.Log("Power Type Not Set");
+            return 0;
+        }
+
+        if (fuelType == "man")
+        {
+            return manCost;
+        }
+
+        if (fuelType == "wind")
+        {
+            return windCost;
+        }
+
+        if (fuelType == "electric")
+        {
+            return electricCost;
+        }
+
+        if (fuelType == "oil")
+        {
+            return oilCost;
+        }
+
+        if (fuelType == "coal")
+        {
+            return coalCost;
+        }
+
+        //BAD 
+        return 0;
+    }
+
+    void CheckWinningPath()
+    {
+        float playerCost = 0;
+        for (int i = 0; i < playerPaths.Length; i++)
+        {
+            playerCost += playerFuels[i] * (playerPaths[i].travelDistance / playerPaths[i].travelSpeed);
+        }
+        if ( Mathf.Floor(playerCost) > powerPoints)
+        {
+            Debug.Log("This path will FAIL");
+            Debug.Log(playerCost);
+        } else
+        {
+            Debug.Log("This path will PASS");
+            Debug.Log(playerCost);
+        }
+            
+    }
+
 }
