7afc0a8db4c360fa9a4d92772331055981e727e8
belegrin <belegrin@email.sc.edu>
2018-04-24 20:37:56 -0400

    Level Select - Level Lock Functionality

--- PowerSwitch2D/Assets/Scripts/LoadSceneOnClick.cs
+++ PowerSwitch2D/Assets/Scripts/LoadSceneOnClick.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LoadSceneOnClick : MonoBehaviour {
-    
+
+    private Button myButton;
+    public int unlockNumber;
+
+    void Start()
+    {
+        myButton = GetComponent<Button>();
+
+        if (PlayerPrefs.GetInt("unlock", 0) < unlockNumber)
+            myButton.interactable = false;
+    }
+
     public void LoadByIndex(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
