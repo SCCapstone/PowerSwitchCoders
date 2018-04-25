cd76e1903aa9b60b4b2f89cd9cd88786d44203a3
William Smith <wlsmith@email.sc.edu>
2018-04-24 22:01:57 -0400

    Final Demo Fixes
    Fixed credits screen button, added script to reset player prefs for demo and final deployment

---
+++ PowerSwitch2D/Assets/Scripts/PrefReset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefReset : MonoBehaviour {
+    public bool doReset = false;
+
+	// Use this for initialization
+	void Start () {
+        if (doReset)
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
